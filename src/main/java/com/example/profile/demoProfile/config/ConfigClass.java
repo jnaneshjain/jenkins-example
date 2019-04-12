911a7d81fddaf631de6ef7d1aac68a004b4e7651
jnaneshjain <jnanesh.jain@arisglobal.com>
2019-04-12 23:34:15 +0530

    Name conflict resolved

--- src/main/java/com/example/profile/demoProfile/config/ConfigClass.java
+++ src/main/java/com/example/profile/demoProfile/config/ConfigClass.java
@@ -1,4 +1,4 @@
-package com.example.profile.demoProfile.Config;
+package com.example.profile.demoProfile.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
